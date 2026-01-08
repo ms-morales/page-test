ae207461ce827c6b4d2aac5b553916fcb2161470
ms-morales <142621753+ms-morales@users.noreply.github.com>
2026-01-08 01:16:31 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-// Este archivo puede ser utilizado para añadir comportamientos interactivos más adelante
-console.log("Página de IoTec Argentina cargada con éxito.");
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+    reveals.forEach(section => {
+        const windowHeight = window.innerHeight;
+        const elementTop = section.getBoundingClientRect().top;
+        const elementVisible = 120;
+
+        if (elementTop < windowHeight - elementVisible) {
+            section.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
